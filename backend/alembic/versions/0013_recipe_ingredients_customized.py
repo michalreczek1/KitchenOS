90a8cc5b571a4fff30ebe66de2ca382d79506cf6
michalreczek1 <michalreczek@gmail.com>
2026-02-28 19:12:24 +0100

    Shorten Alembic revision id

--- backend/alembic/versions/0013_recipe_ingredients_customized.py
+++ backend/alembic/versions/0013_recipe_ingredients_customized.py
@@ -1,6 +1,6 @@
 """add recipe ingredients customized flag
 
-Revision ID: 0013_recipe_ingredients_customized
+Revision ID: 0013_recipe_ing_custom
 Revises: 0012_nutrition_resilient
 Create Date: 2026-02-28 19:05:00.000000
 """
@@ -9,7 +9,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0013_recipe_ingredients_customized"
+revision = "0013_recipe_ing_custom"
 down_revision = "0012_nutrition_resilient"
 branch_labels = None
 depends_on = None
